25e02e93b8f3ee2bc90bfcf3b19fb7562f97780c
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-17 19:49:12 +0000

    added navbar links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,16 @@ import React from "react";
 export default function NavBar (){
     return(
         <div className="text-6 bg-blue-700">
-            navbar
+            <nav>
+
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">New</a></li>
+                    <li><a href="">Popular</a></li>
+                    <li><a href="">trending</a></li>
+                    <li><a href="">Categories</a></li>
+                </ul>
+            </nav>
         </div>
     )
 }
