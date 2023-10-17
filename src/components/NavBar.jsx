8142b7e8a137bd43fc61c9d240914465c0733ce2
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-17 19:55:16 +0000

    done with the skeleton of the navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,19 +3,16 @@ import logo from '../assets/logo.svg'
 
 export default function NavBar (){
     return(
-        <div className="text-6 bg-blue-700">
-            <nav>
-                <div>
-                    <img src={logo} alt="" />
-                </div>
-                <ul>
-                    <li><a href="">Home</a></li>
-                    <li><a href="">New</a></li>
-                    <li><a href="">Popular</a></li>
-                    <li><a href="">trending</a></li>
-                    <li><a href="">Categories</a></li>
-                </ul>
-            </nav>
-        </div>
+    <nav className="flex items-center justify-between">
+        <img src={logo} alt="" />
+
+        <ul className="flex items-center gap-2">
+            <li><a href="">Home</a></li>
+            <li><a href="">New</a></li>
+            <li><a href="">Popular</a></li>
+            <li><a href="">trending</a></li>
+            <li><a href="">Categories</a></li>
+        </ul>
+    </nav>    
     )
 }
