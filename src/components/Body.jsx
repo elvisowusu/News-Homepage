1b4af67f0f1eef2ed9a3ba82a5b841aa51d1d67c
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-17 20:08:45 +0000

    imported assets

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import desktopImg from '../assets/image-web-3-desktop.jpg'
 import mobileImg from '../assets/image-web-3-mobile.jpg'
-
+import gamingImg from '../assets/image-gaming-growth.jpg'
+import retroImg from '../assets/image-retro-pcs.jpg'
+import laptopImg from '../assets/image-top-laptops.jpg'
 export default function Body (){
     return(
         <div className="">
