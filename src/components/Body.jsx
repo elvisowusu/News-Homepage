5c1e4a2436b3b842abb1bd23ed77670a15492bc0
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-17 20:05:56 +0000

    imported desktop and mobile images

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import desktopImg from '../assets/image-web-3-desktop.jpg'
+import mobileImg from '../assets/image-web-3-mobile.jpg'
 
 export default function Body (){
     return(
-        <div className="bg-red-500">
-            Body 
+        <div className="">
+            <section>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+            </section>
+            <section>
+                <div></div>
+                <div></div>
+                <div></div>
+            </section>
         </div>
     )
 }
