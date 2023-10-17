dfdeb70a266f9be50347fbbf8d63e92859e68db7
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-17 22:37:01 +0000

    done with the text-colors

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -35,24 +35,24 @@ export default function Body (){
 
                 {/* {rightSide} */}
                 <div className="bg-veryDarkBlue">
-                    <h1 className="text">New</h1>
+                    <h1 className="text-softOrange">New</h1>
                     <div>
-                        <h3>Hydrogen VS Electric Cars</h3>
-                        <p>
+                        <h3 className="text-white">Hydrogen VS Electric Cars</h3>
+                        <p className="text-darkGrayishBlue">
                             Will hydrogen-fueled cars ever catch up to EVs?
                         </p>
                     </div>
                     <hr />
                     <div>
-                        <h3>The Downsides of AI Artistry</h3>
-                        <p>
+                        <h3 className="text-white">The Downsides of AI Artistry</h3>
+                        <p className="text-darkGrayishBlue">
                             What are the possible adverse effects of on-demand AI image generation?
                         </p>
                     </div>
                     <hr />
                     <div>
-                        <h3>Is VC Funding Drying Up?</h3>
-                        <p>
+                        <h3 className="text-white">Is VC Funding Drying Up?</h3>
+                        <p className="text-darkGrayishBlue">
                             Private funding by VC firms is down 50% YOY. We take a look at what that means.
                         </p>
                     </div>
