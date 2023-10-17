641418705367c5bff68eee3ab9a69e6ea22314bf
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-17 22:51:19 +0000

    added more styles to the navBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,12 +6,12 @@ export default function NavBar (){
     <nav className="flex items-center justify-between w-[150vh] mb-12">
         <img src={logo} alt="" />
 
-        <ul className="flex items-center gap-10">
-            <li><a href="">Home</a></li>
-            <li><a href="">New</a></li>
-            <li><a href="">Popular</a></li>
-            <li><a href="">trending</a></li>
-            <li><a href="">Categories</a></li>
+        <ul className="flex items-center gap-10 text-darkGrayishBlue">
+            <li className="hover:text-softRed transition ease-in duration-150"><a href="">Home</a></li>
+            <li className="hover:text-softRed transition ease-in duration-150"><a href="">New</a></li>
+            <li className="hover:text-softRed transition ease-in duration-150"><a href="">Popular</a></li>
+            <li className="hover:text-softRed transition ease-in duration-150"><a href="">trending</a></li>
+            <li className="hover:text-softRed transition ease-in duration-150"><a href="">Categories</a></li>
         </ul>
     </nav>    
     )
