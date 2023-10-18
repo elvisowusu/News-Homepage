f14a4bc43d18a08d4835b89aa036eac1bad92b3c
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-18 00:25:30 +0000

    more styles

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -19,7 +19,7 @@ export default function Body (){
                         src={screenWidth < 641?mobileImg: desktopImg} alt="" 
                     />
 
-                    <div className="sm:flex sm:justify-evenly mt-6">
+                    <div className="sm:flex sm:justify-evenly mt-7">
                         
                         <h1 className="font-extrabold text-veryDarkBlue text-[2.8rem] leading-[2.7rem]">
                             The Bright Future of Web 3.0?
@@ -30,7 +30,7 @@ export default function Body (){
                                 We dive into the next evolution of the web that 
                                 claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?
                             </p>
-                            <button className="bg-softRed text-offWhite text-xs font-bold w-[11.5rem] py-[1.2rem] mt-5 tracking-[0.4rem]">
+                            <button className="bg-softRed text-offWhite text-xs font-bold w-[11.5rem] py-[1.2rem] mt-6 tracking-[0.4rem]">
                                 READ MORE
                             </button>
                         </div>
