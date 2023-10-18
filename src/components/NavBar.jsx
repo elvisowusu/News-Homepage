a4f49553f3e309ab7f81927ae79662287d09dedd
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-18 14:26:54 +0000

    finished navbar style for sm devices

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -47,7 +47,7 @@ export default function NavBar (){
                     </>
             :
 
-            <ul className="flex items-center gap-10 text-darkGrayishBlue">
+            <ul className="flex items-center gap-10 sm:gap-5 text-darkGrayishBlue">
                 <li className="hover:text-softRed transition ease-in duration-150"><a href="">Home</a></li>
                 <li className="hover:text-softRed transition ease-in duration-150"><a href="">New</a></li>
                 <li className="hover:text-softRed transition ease-in duration-150"><a href="">Popular</a></li>
